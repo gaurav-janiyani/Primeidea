bb8aacca92217bf467a91ac48f63e5ea2f3cb03b
Gaurav Janiyani <gauravjaniyani@gmail.com>
2024-10-07 15:04:44 +0530

    footer updates

--- components/footer.js
+++ components/footer.js
@@ -27,9 +27,9 @@ const Footer = () => {
           <span className="absolute bottom-0 right-40 z-[999]">
             <Image src="/images/footer/shape1.png" width="300" height="190" alt="Shape" />
           </span>
-          <span className="absolute right-0 top-10 z-[999]">
+          {/* <span className="absolute right-0 top-10 z-[999]">
             <Image src="/images/footer/shape2.png" width="81" height="127" alt="Shape" />
-          </span>
+          </span> */}
         </div>
         <div className="relative z-[9999] border border-solid border-[#B4B4B4]">
           <div className="lg:max-w-[1024px] xl:max-w-[1170px] 2xl:max-w-[1340px] mx-auto px-4">
@@ -48,7 +48,7 @@ const Footer = () => {
                 <div className="absolute top-0 right-0 bg-[#3E8ABD] w-[400%] h-[100%] border-r border-solid border-[#B4B4B4] z-[-1]">
                 </div>
                 <div className="mb-10 w-full">
-                  <a href="/#" className="mb-6 inline-block lg:max-w-[260px] xl:max-w-[280px] 2xl:max-w-[320px]">
+                  <a href="/" className="mb-6 inline-block lg:max-w-[260px] xl:max-w-[280px] 2xl:max-w-[320px]">
                     <Image
                       src="/images/logo-white.png"
                       width={320}
@@ -61,24 +61,24 @@ const Footer = () => {
               </div>
               <div className=" pb-20 pt-12 w-full pl-14 pr-10 sm:w-1/2 lg:max-w-[42%] xl:max-w-[40%] 2xl:max-w-[33.33%]">
                 <LinkGroup header="Wealth Creation">
-                  <NavLink link="/#" label="Financial Investment Advisory" />
-                  <NavLink link="/#" label="Financial & Investment Planning" />
+                  {/* <NavLink link="/#" label="Financial Investment Advisory" /> */}
+                  <NavLink link="/financial-planning-and-investment-advisory" label="Financial Planning & Investment Advisory" />
                 </LinkGroup>
                 <LinkGroup header="Wealth Preservation">
-                  <NavLink link="/#" label="Research Based Wealth Management" />
-                  <NavLink link="/#" label="Dream Retirement Planning" />
-                  <NavLink link="/#" label="Legacy & Inheritance Planning" />
-                  <NavLink link="/#" label="Setting Tax Planning & Savings" />
+                  <NavLink link="/research-based-wealth-management" label="Research Based Wealth Management" />
+                  <NavLink link="/retirement-planning" label="Dream Retirement Planning" />
+                  <NavLink link="/legacy-inheritance-planning" label="Legacy & Inheritance Planning" />
+                  <NavLink link="/tax-planning-savings" label="Setting Tax Planning & Savings" />
                 </LinkGroup>
               </div>
 
               <div className=" pb-20 pt-12 w-full px-4 sm:w-1/2 lg:max-w-[20%] xl:max-w-[25%] 2xl:max-w-[25%]">
                 <div className="mb-10 w-full">
                   <h4 className="mb-3 text-lg font-semibold text-white">
-                    <a href="">Insurance</a>
+                    <a href="/insurance">Insurance</a>
                   </h4>
                   <h4 className="mb-3 text-lg font-semibold text-white">
-                    <a href="">About Us</a>
+                    <a href="/about-us">About Us</a>
                   </h4>
                   <h4 className="mb-3 text-lg font-semibold text-white">
                     <a href="">Blogs</a>
