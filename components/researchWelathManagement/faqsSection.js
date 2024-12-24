414626b6f0ce437c5f421b1d6eebb25e99edcfc4
Gaurav Janiyani <gauravjaniyani@gmail.com>
2024-12-24 18:00:58 +0530

    research based wealth management page faqs section content add

---
+++ components/researchWelathManagement/faqsSection.js
@@ -0,0 +1,122 @@
+"use client"
+import { useState } from "react";
+import Image from "next/image";
+import FadeUpOneByOneAnimation from "@/animations/FadeUpOneByOneAnimation";
+
+const FaqsSection = () => {
+    const [activeTab, setActiveTab] = useState(0);
+
+    const handleTabClick = (index) => {
+        setActiveTab(index === activeTab ? -1 : index);
+    };
+    return (
+        <div className="py-16 sm:py-8">
+            <div className="mx-auto 2xl:max-w-[1340px] xl:max-w-[1170px] lg:max-w-[1004px] px-4">
+              <FadeUpOneByOneAnimation className="text-center max-w-[767px] px-4 mx-auto">
+                <h2 className="text-2xl font-medium tracking-tight mb-2 md:mb-4 text-[#04102A] sm:text-3xl">
+                  Frequently Asked Questions
+                </h2>
+                <p className="mb-12 text-base font-semibold leading-6 text-[#4D4D4D]">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                </p>
+              </FadeUpOneByOneAnimation>
+              <div className="max-w-[1023px] mx-auto">
+                <ul>
+                  <li className="bg-[#C6E8FF] p-4 md:p-6 rounded-lg cursor-pointer" onClick={() => handleTabClick(0)}>
+                    <FadeUpOneByOneAnimation>
+                    <h2 className="flex items-center justify-between text-[18px] md:text-[20px] 2xl:text-[22px] font-semibold text-[#222222]">
+                      <span>
+                        What is research-based wealth management?
+                      </span>
+                      <Image src="/images/icons/circle-down-arrow.png" width={30} height={30} alt="Arrow" className="w-[24px] sm:w-[30px] h-[24px] sm:h-[30px] ml-2" />
+                    </h2>
+                    <div className={`content mt-4 text-base md:text-lg max-w-full md:max-w-[85%] text-[#222222] ${activeTab === 0 ? 'block' : 'hidden'}`}>
+                      <p>
+                        Research-based wealth management is a data-driven approach to managing wealth, leveraging in-depth market analysis, economic trends, and financial models to optimize investment strategies and asset allocation.
+                      </p>
+                    </div>
+                    </FadeUpOneByOneAnimation>
+                  </li>
+                  <li className="bg-[#C6E8FF] p-4 md:p-6 rounded-lg cursor-pointer" onClick={() => handleTabClick(1)}>
+                    <FadeUpOneByOneAnimation>
+                    <h2 className="flex items-center justify-between text-[18px] md:text-[20px] 2xl:text-[22px] font-semibold text-[#222222]">
+                      <span>
+                        How it is different from traditional wealth management?
+                      </span>
+                      <Image src="/images/icons/circle-down-arrow.png" width={30} height={30} alt="Arrow" className="w-[24px] sm:w-[30px] h-[24px] sm:h-[30px] ml-2" />
+                    </h2>
+                    <div className={`content mt-4 text-base md:text-lg max-w-full md:max-w-[85%] text-[#222222] ${activeTab === 1 ? 'block' : 'hidden'}`}>
+                      <p>
+                        While traditional wealth management often relies on experience and general practices, research-based wealth management uses advanced tools, real-time data, and analytics to make evidence-backed investment decisions.
+                      </p>
+                    </div>
+                    </FadeUpOneByOneAnimation>
+                  </li>
+                  <li className="bg-[#C6E8FF] p-4 md:p-6 rounded-lg cursor-pointer" onClick={() => handleTabClick(2)}>
+                    <FadeUpOneByOneAnimation>
+                    <h2 className="flex items-center justify-between text-[18px] md:text-[20px] 2xl:text-[22px] font-semibold text-[#222222]">
+                      <span>
+                        Who can benefit from wealth management services?
+                      </span>
+                      <Image src="/images/icons/circle-down-arrow.png" width={30} height={30} alt="Arrow" className="w-[24px] sm:w-[30px] h-[24px] sm:h-[30px] ml-2" />
+                    </h2>
+                    <div className={`content mt-4 text-base md:text-lg max-w-full md:max-w-[85%] text-[#222222] ${activeTab === 2 ? 'block' : 'hidden'}`}>
+                      <p>
+                        Individuals, families, and businesses with significant assets or complex financial needs can benefit, especially those seeking long-term growth, tax efficiency, and tailored financial strategies.
+                      </p>
+                    </div>
+                    </FadeUpOneByOneAnimation>
+                  </li>
+                  <li className="bg-[#C6E8FF] p-4 md:p-6 rounded-lg cursor-pointer" onClick={() => handleTabClick(3)}>
+                    <FadeUpOneByOneAnimation>
+                    <h2 className="flex items-center justify-between text-[18px] md:text-[20px] 2xl:text-[22px] font-semibold text-[#222222]">
+                      <span>
+                        What types of investment products are included in wealth management?
+                      </span>
+                      <Image src="/images/icons/circle-down-arrow.png" width={30} height={30} alt="Arrow" className="w-[24px] sm:w-[30px] h-[24px] sm:h-[30px] ml-2" />
+                    </h2>
+                    <div className={`content mt-4 text-base md:text-lg max-w-full md:max-w-[85%] text-[#222222] ${activeTab === 3 ? 'block' : 'hidden'}`}>
+                      <p>
+                        Investment products may include stocks, bonds, mutual funds, ETFs, real estate, private equity, and alternative investments like commodities or hedge funds, based on your goals and research findings.
+                      </p>
+                    </div>
+                    </FadeUpOneByOneAnimation>
+                  </li>
+                  <li className="bg-[#C6E8FF] p-4 md:p-6 rounded-lg cursor-pointer" onClick={() => handleTabClick(4)}>
+                    <FadeUpOneByOneAnimation>
+                    <h2 className="flex items-center justify-between text-[18px] md:text-[20px] 2xl:text-[22px] font-semibold text-[#222222]">
+                      <span>
+                        Can research-based wealth management help with tax optimization?
+                      </span>
+                      <Image src="/images/icons/circle-down-arrow.png" width={30} height={30} alt="Arrow" className="w-[24px] sm:w-[30px] h-[24px] sm:h-[30px] ml-2" />
+                    </h2>
+                    <div className={`content mt-4 text-base md:text-lg max-w-full md:max-w-[85%] text-[#222222] ${activeTab === 4 ? 'block' : 'hidden'}`}>
+                      <p>
+                        Yes, it includes strategies to minimize tax liabilities through efficient asset allocation, timing of transactions, and utilizing tax-advantaged investment accounts.
+                      </p>
+                    </div>
+                    </FadeUpOneByOneAnimation>
+                  </li>
+                  <li className="bg-[#C6E8FF] p-4 md:p-6 rounded-lg cursor-pointer" onClick={() => handleTabClick(5)}>
+                    <FadeUpOneByOneAnimation>
+                    <h2 className="flex items-center justify-between text-[18px] md:text-[20px] 2xl:text-[22px] font-semibold text-[#222222]">
+                      <span>
+                        How does it ensure long-term financial growth?
+                      </span>
+                      <Image src="/images/icons/circle-down-arrow.png" width={30} height={30} alt="Arrow" className="w-[24px] sm:w-[30px] h-[24px] sm:h-[30px] ml-2" />
+                    </h2>
+                    <div className={`content mt-4 text-base md:text-lg max-w-full md:max-w-[85%] text-[#222222] ${activeTab === 5 ? 'block' : 'hidden'}`}>
+                      <p>
+                        Through meticulous research, strategic asset allocation, active monitoring, and risk management, it balances immediate returns with sustainable, long-term wealth creation.
+                      </p>
+                    </div>
+                    </FadeUpOneByOneAnimation>
+                  </li>
+                </ul>
+              </div>
+            </div>
+      </div>
+    )
+ }
+
+ export default FaqsSection;
