aa6b6ccf7f3b9e075fdce6b3cad00e257e39e89b
Gaurav Janiyani <gauravjaniyani@gmail.com>
2025-01-10 18:58:36 +0530

    updated chatbot code

--- app/layout.js
+++ app/layout.js
@@ -72,35 +72,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en"  className={`${manrope.variable}`}>
-      <body
-        className={manrope.className}
-      >
+    <html lang="en" className={`${manrope.variable}`}>
+      <body className={manrope.className}>
         {children}
-        <Script id="yellow-ai" strategy="afterInteractive">
-          {`
-            window.ymConfig = {"bot":"x1734168923745","host":"https://r0.cloud.yellow.ai"};
-            (function() {
-                var w = window,
-                    ic = w.YellowMessenger;
-                if ("function" === typeof ic) ic("reattach_activator"), ic("update", ymConfig);
-                else {
-                    var d = document,
-                        i = function() {
-                            i.c(arguments)
-                        };
-                    function l() {
-                        var e = d.createElement("script");
-                        e.type = "text/javascript", e.async = !0, e.src = "https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js";
-                        var t = d.getElementsByTagName("script")[0];
-                        t.parentNode.insertBefore(e, t)
-                    }
-                    i.q = [], i.c = function(e) {
-                        i.q.push(e)
-                    }, w.YellowMessenger = i, w.attachEvent ? w.attachEvent("onload", l) : w.addEventListener("load", l, !1)}
-            })();
-          `}
+        <Script 
+          id="yellow-ai-config" 
+          strategy="beforeInteractive"
+        >
+          {`window.ymConfig = {"bot":"x1734168923745","host":"https://r0.cloud.yellow.ai"};`}
         </Script>
+        <Script
+          id="yellow-ai-loader"
+          strategy="afterInteractive"
+          src="https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js"
+        />
       </body>
     </html>
   );
