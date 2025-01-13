9f7b845a9cbc0181b3b53b9d8f928d4c15e7f905
Gaurav Janiyani <gauravjaniyani@gmail.com>
2025-01-13 11:00:21 +0530

    chatbot updates

--- components/chatbot.js
+++ components/chatbot.js
@@ -1,36 +1,13 @@
 'use client';
 
-import Script from "next/script";
+import Script from 'next/script';
 
 export const ChatBotScripts = () => {
-
     return (
         <Script
-            id="yellow-ai-config"
-            strategy="beforeInteractive"
-            onLoad={() => {
-
-            }}
-        >
-            {` window.ymConfig = { "bot": "x1734168923745", "host": "https://r0.cloud.yellow.ai" };
-                var w = window,
-                    ic = w.YellowMessenger;
-                if ("function" === typeof ic) ic("reattach_activator"), ic("update", ymConfig);
-                else {
-                    var d = document,
-                        i = function () {
-                            i.c(arguments)
-                        };
-                    function l() {
-                        var e = d.createElement("script");
-                        e.type = "text/javascript", e.async = !0, e.src = "https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js";
-                        var t = d.getElementsByTagName("script")[0];
-                        t.parentNode.insertBefore(e, t)
-                    }
-                    i.q = [], i.c = function (e) {
-                        i.q.push(e)
-                    }, w.YellowMessenger = i, w.attachEvent ? w.attachEvent("onload", l) : w.addEventListener("load", l, !1)
-                }`}
-        </Script>
-    )
+            src="/static/yellowai.js"
+            strategy="afterInteractive"
+            id="yellow-messenger"
+        />
+    );
 }
