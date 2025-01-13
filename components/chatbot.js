b6237f6ba2a3a0ef4e5500aaeae9d755a36d073f
Gaurav Janiyani <gauravjaniyani@gmail.com>
2025-01-13 11:29:49 +0530

    Revert "test commit" revert commit This reverts commit b52486f187ead2259ed983adf8110061ab18ea91.

--- components/chatbot.js
+++ components/chatbot.js
@@ -1,13 +1,45 @@
 'use client';
 
+<<<<<<< HEAD
 import Script from 'next/script';
 
+=======
+>>>>>>> parent of b52486f (test commit)
 export const ChatBotScripts = () => {
     return (
         <Script
+<<<<<<< HEAD
             src="/static/yellowai.js"
             strategy="afterInteractive"
             id="yellow-messenger"
         />
     );
+=======
+            id="yellow-ai-config"
+            strategy="beforeInteractive"
+            onLoad={() => {
+                window.ymConfig = { "bot": "x1734168923745", "host": "https://r0.cloud.yellow.ai" };
+                var w = window,
+                    ic = w.YellowMessenger;
+                if ("function" === typeof ic) ic("reattach_activator"), ic("update", ymConfig);
+                else {
+                    var d = document,
+                        i = function () {
+                            i.c(arguments)
+                        };
+                    function l() {
+                        var e = d.createElement("script");
+                        e.type = "text/javascript", e.async = !0, e.src = "https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js";
+                        var t = d.getElementsByTagName("script")[0];
+                        t.parentNode.insertBefore(e, t)
+                    }
+                    i.q = [], i.c = function (e) {
+                        i.q.push(e)
+                    }, w.YellowMessenger = i, w.attachEvent ? w.attachEvent("onload", l) : w.addEventListener("load", l, !1)
+                }
+            }}
+        >
+        </Script>
+    )
+>>>>>>> parent of b52486f (test commit)
 }
